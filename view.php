c48550956e058f138e6b2671839921d237b94e4b
deepak <sahdeepak83@gmail.com>
2018-06-28 21:56:09 +0530

    commit view

--- view.php
+++ view.php
@@ -1,65 +1,23 @@
 <?php
 // include core and response files
-	include("config/core.php");
-	include("response.php");
+include("config/core.php"); 
+include("config/database.php");
+	$db = new dbObj();
+$connString =  $db->getConnstring();
+$conn= $connString;
 	/*$jsonD='{
 				"token": "1609dd95b29704c37faebf92a64fcde6",
 				"query": "Select id,name from public.individual_tip_non_tip_events__c where name=\'t1\'"
 			}';*/
+	/*$jsonD='{
+	"token": "b3404a00cdfc1eef0f89fae6cac5df04",
+	"query": "select id,name,tip_lat_lon__latitude__s, tip_lat_lon__longitude__s from Individual_Tip_Non_Tip_Events__c"
+}';*/
 	$jsonD = $GLOBALS['HTTP_RAW_POST_DATA'];		
 	$jsonResult=json_decode($jsonD,true);
-	// instantiate token and  object
-	if(isset($jsonResult['token'])){
-		$newObj = new Dbdata();
-		$gettoken=trim($jsonResult['token']);
-		$tokenData = $newObj->getToken($gettoken);
-			if(!empty($tokenData)){
-				foreach($tokenData as $key => $tok) {
-					$tokenVal=$tok;
-				}
-				$matchedToken=$tokenVal['token'];
-			}else{
-				$matchedToken='false';
-			}
-		
-	}else{
-		echo json_encode(
-						array("message" => "Invalid Token Parameter.")
+	$querytest ="INSERT INTO public.api_call (xmlformat) VALUES ('".$jsonD."')";
+			 pg_query($conn, $querytest) or die("error to save  data"); 
+	echo json_encode(
+						array("message" => $jsonResult)
 					  );
-					  return ;
-	}
-	$gettoken=trim($jsonResult['token']);
-		//check conditions basis of token trure or false
-	if(isset($matchedToken) && $matchedToken!="false" && $matchedToken==$gettoken){ 
-       // for check condition basis of urls and redirect to functions
-	
-			if(isset($jsonResult['query'])) {
-				// instantiate database and  object
-				$newObj = new Dbdata();
-				$data = $newObj->getAllWithQuery($jsonResult['query']);
-				// products array
-				$products_arr=array();
-				$products_arr["results"]=array();
-					if(!empty($data)){
-						foreach($data as $key => $emp) {
-							array_push($products_arr["results"], $emp);
-						}
-					// make it json format	
-					echo json_encode($products_arr);
-					}else{
-						echo json_encode(
-						array("message" => "No data found.")
-					  );
-					}
-				
-			}
-	}else{
-		echo json_encode(
-						array("message" => "Unauthorized Token || Expired Token")
-					  );
-	}
-    //delete token
-	$newObj = new Dbdata();
-	$tokenData = $newObj->delToken($gettoken);
- ?>
-
+	?>
