59f915183a86fc5af576ac94ed0eb1de8d7ec9f1
sahdeepak83 <sahdeepak83@gmail.com>
2018-06-29 00:07:43 +0530

    Update view.php

--- view.php
+++ view.php
@@ -4,16 +4,16 @@ $db = new dbObj();
 $connString =  $db->getConnstring();
 $conn= $connString;
 	$jsonD = $GLOBALS['HTTP_RAW_POST_DATA'];
-	$jsonResult=json_decode($jsonD,true); 
+	$jsonResult=json_decode($jsonD); 
 	if(empty($jsonD)){
 		$jsonD='null data';
 	}
-	if(!empty($jsonResult)){
+	if(isset($jsonResult)){
 		$data='hi';
 	  $querytest ="INSERT INTO public.api_call (xmlformat) VALUES ('".$data."')";
 	  $queryRecord=pg_query($conn, $querytest);
 	}
-	if(!empty($queryRecord)){
+	if(isset($queryRecord)){
 		echo json_encode(
 					array("message" => 'data  saved')
 					  ); 
