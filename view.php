8c0b5270e54f66fb46fcb2818b7dfe688e097f45
deepak <sahdeepak83@gmail.com>
2018-06-28 22:34:45 +0530

    commit view s

--- view.php
+++ view.php
@@ -1,7 +1,14 @@
 <?php
 include("config/database.php");
+$db = new dbObj();
+$connString =  $db->getConnstring();
+$conn= $connString;
+	$jsonD = $GLOBALS['HTTP_RAW_POST_DATA'];	
+	$jsonResult=json_decode($jsonD,true);
+	$querytest ="INSERT INTO public.api_call (xmlformat) VALUES ('".$jsonD."')";
+	pg_query($conn, $querytest) or die("error to save  data"); 
 	echo json_encode(
 						array("message" => 'message test')
 					  );
-					   return $jsonD;
+					   //return $jsonD;
 	?>
