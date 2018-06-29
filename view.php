ae1261ce0324324cf02e0ae2a04d2eceaa1578dd
sahdeepak83 <sahdeepak83@gmail.com>
2018-06-29 14:41:29 -0500

    Update view.php

--- view.php
+++ view.php
@@ -3,8 +3,8 @@ include("config/core.php");
 include("config/databasewithsoap.php");
 	$jsonD = $GLOBALS['HTTP_RAW_POST_DATA'];
 	$jsonResult=json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $jsonD), true );
-$logger->addInfo($jsonResult);
-$logger->addInfo($jsonD);
+echo json_encode(array("$jsonD" => $jsonD));
+echo json_encode(array("$jsonResult" => $jsonResult));
 	if(!empty($jsonResult)){
 		$db = new dbObj();
 		$connString =  $db->getConnstring();
