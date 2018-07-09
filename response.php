b162c4fe2fb1e1898b6c325e289e6de9d69b31cc
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-09 23:25:01 +0530

    Update response.php

--- response.php
+++ response.php
@@ -54,7 +54,7 @@ class Dbdata {
 		$token=trim($token);
  		$createddate=date('Y-m-d h:i:s');
         $token_expire=date('Y-m-d H:i:s',strtotime('5 minutes',strtotime($createddate)));			
-		pg_query("INSERT INTO public.token(token, token_expire, createddate) VALUES('".$token."', '".$token_expire."', '".$createddate."')");
+		pg_query($this->conn,"INSERT INTO public.token(token, token_expire, createddate) VALUES('".$token."', '".$token_expire."', '".$createddate."')");
 		
 	}
 	
