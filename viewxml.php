5bfdba80d023ce9d5426e256ab0a1231e03a2b5a
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-09 20:49:29 +0530

    Update viewxml.php

--- viewxml.php
+++ viewxml.php
@@ -1,1 +1,17 @@
+<?php  
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+include("config/databasewithsoap.php");
+$db = new dbObj();
+$connString =  $db->getConnstring();
+$conn= $connString;
+$xmlOutput = file_get_contents('php://input');
+//for save xml get data
+//$querytest ="INSERT INTO public.api_call (xmlformat) VALUES ('".$xmlOutput."')";
+			 //pg_query($conn, $querytest) or die("error to save  data"); 
 
+
+$xml = simplexml_load_string($xmlOutput);
+ echo "<pre>";print_r($xml);die;
+
+ ?>
