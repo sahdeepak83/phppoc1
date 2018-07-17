90c96f952c90a701e0ef9636252234b772044541
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-17 13:51:49 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,6 @@ include("config/core.php");
 Class dbObj{
 	
 	// specify your own database credentials
-	var $conn;
 	/*
 	var $servername = "ec2-54-83-1-94.compute-1.amazonaws.com";
 	var $username = "trtclzwzsolgjp";
@@ -12,14 +11,23 @@ Class dbObj{
 	var $port = "5432";
 	var $sslmode= "require";
 	*/
-	
+	/*
 	function pg_connection_string_from_database_url() {
   		extract(parse_url($_ENV["DATABASE_URL"]));
 		echo json_encode(
 				array("user=$user password=$pass host=$host dbname=" . substr($path, 1) . "port=$port sslmode=$sslmode")
 			  );
   		return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . "port=$port sslmode=$sslmode"; # <- you may want to add sslmode=require there too
-	}
+	}*/
+	
+	$parts = parse_url($url_string);
+	var $servername = $parts['host'];
+	var $username = $parts['user'];
+	var $password = $parts['pass'];
+	var $dbname = substr($parts['path'], 1);
+	var $port = $parts['port'];
+	var $sslmode= "require";
+	var $conn;
 	  /*
 	 Function Name: getConnstring
 	 Params: NULL
@@ -28,12 +36,14 @@ Class dbObj{
 	 Description : connection for Postgres server in Heroku
     	*/
 	function getConnstring() { 
-		//$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
-    	/*	extract(parse_url($_ENV["DATABASE_URL"]));
+		$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
+    	/*	
+		//$con = pg_connect(pg_connection_string_from_database_url());
+		extract(parse_url($_ENV["DATABASE_URL"]));
   		$url = "user=$user password=$pass host=$host dbname=" . substr($path, 1);  
 		$con = pg_connect(url);
 	*/	
-		$con = pg_connect(pg_connection_string_from_database_url());
+		
 		/* check connection */
 		if (pg_last_error()) {
 			 echo json_encode(
