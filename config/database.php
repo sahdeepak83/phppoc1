168edcce1b7701ea9be5b9f71d56546f4f1fe97c
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-17 14:31:03 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,6 @@ include("config/core.php");
 Class dbObj{
 	
 	// specify your own database credentials
-	/*
 	var $servername = "ec2-54-83-1-94.compute-1.amazonaws.com";
 	var $username = "trtclzwzsolgjp";
 	var $password = "67390df03544f17e1db60bdb91c8650501d56f0c4b5267b475d3408ce47315e8";
@@ -11,7 +10,7 @@ Class dbObj{
 	var $port = "5432";
 	var $conn;
 	var $sslmode= "require";
-	*/
+	/*
 	var $conn;
 	function pg_connection_string_from_database_url() {
   		extract(parse_url($_ENV["DATABASE_URL"]));
@@ -37,14 +36,14 @@ Class dbObj{
 	 Description : connection for Postgres server in Heroku
     	*/
 	function getConnstring() { 
-		//$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
+		$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
     	/*	
 		//$con = pg_connect(pg_connection_string_from_database_url());
 		extract(parse_url($_ENV["DATABASE_URL"]));
   		$url = "user=$user password=$pass host=$host dbname=" . substr($path, 1);  
 		$con = pg_connect(url);
 	*/	
-		$con = pg_connect(pg_connection_string_from_database_url());
+		//$con = pg_connect(pg_connection_string_from_database_url());
 		/* check connection */
 		if (pg_last_error()) {
 			 echo json_encode(
