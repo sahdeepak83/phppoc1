562f6292787c09c83c02c4f4e7b91eacaa08f7a7
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-17 11:48:42 -0500

    Update databasewithsoap.php

--- config/databasewithsoap.php
+++ config/databasewithsoap.php
@@ -2,7 +2,6 @@
 Class dbObj{
 	
 	// specify your own database credentials
-	/*
 	var $servername = "ec2-54-83-1-94.compute-1.amazonaws.com";
 	var $username = "trtclzwzsolgjp";
 	var $password = "67390df03544f17e1db60bdb91c8650501d56f0c4b5267b475d3408ce47315e8";
@@ -10,11 +9,11 @@ Class dbObj{
 	var $port = "5432";
 	var $conn;
 	var $sslmode= "require";
-	*/
+	/*
 	function pg_connection_string_from_database_url() {
   		extract(parse_url($_ENV["DATABASE_URL"]));
   		return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-	}
+	}*/
 	  /*
 	 Function Name: getConnstring
 	 Params: NULL
@@ -24,8 +23,8 @@ Class dbObj{
     */
 	function getConnstring() { 
 		//Connecting to database
-		//$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
-     		$con = pg_connect(pg_connection_string_from_database_url());
+		$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
+     		//$con = pg_connect(pg_connection_string_from_database_url());
 		/* check connection */
 		if (pg_last_error()) {
 			 echo json_encode(
