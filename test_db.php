7cdc39b6c424e67a85af0ce8c494ceda386d6a9e
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-22 11:45:01 +0530

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,22 +1,20 @@
 <?php
-var $conn;
-	function pg_connection_string_from_database_url() {
-  		extract(parse_url($_ENV["DATABASE_URL"]));
-  		return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . "port=$port sslmode=require"; # <- you may want to add sslmode=require there too
-	}
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
-echo "<pre>";print_r($pg_conn );die;
-	/*
-	$parts = parse_url($_ENV["DATABASE_URL"]);
-	echo json_encode(
-				array("$parts")
-			  );
-	var $servername = $parts['host'];
-	var $username = $parts['user'];
-	var $password = $parts['pass'];
-	var $dbname = substr($parts['path'], 1);
-	var $port = $parts['port'];
-	var $sslmode= "require";
-	var $conn;
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
 ?>
