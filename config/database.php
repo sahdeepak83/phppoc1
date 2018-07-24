fbf1e51a1a5e389eebe028a7ac600718825855fd
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-24 10:13:21 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -12,7 +12,7 @@ Class dbObj{
 	var $conn;
 	var $sslmode= extract(parse_url($_ENV["sslmode"]));
 	*/
-	var $servername = "ec2-54-83-1-94.compute-1.amazonaws.com";
+	var $servername = getenv('servername');
 	var $username = "trtclzwzsolgjp";
 	var $password = "67390df03544f17e1db60bdb91c8650501d56f0c4b5267b475d3408ce47315e8";
 	var $dbname = "dduntehkvcp8cu";
