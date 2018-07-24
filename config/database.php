ce6a001c1bd7e2b8878043c0257177c23e911be7
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-24 09:45:06 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -20,7 +20,6 @@ Class dbObj{
 	var $conn;
 	var $sslmode= "require";
 	
-	var $conn;
 	function pg_connection_string_from_database_url() {
   		extract(parse_url($_ENV["DATABASE_URL"]));
   		return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . "port=$port sslmode=require"; # <- you may want to add sslmode=require there too
