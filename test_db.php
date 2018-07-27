0082ca43220e7f29da231c4c590cbfd9418ecb56
sahdeepak83 <sahdeepak83@gmail.com>
2018-07-27 18:36:49 +0530

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,20 +1,44 @@
-<?php
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
+<?php 
+
+	
+	// specify your own database credentials
+	var $servername;
+	var $username;
+	var $password;
+	var $dbname;
+	var $port;
+	var $conn;
+	var $sslmode;
+	function __construct() {
+		$this->servername = getenv('servername');
+		$this->username = getenv('username');
+		$this->password = getenv('password');
+		$this->dbname = getenv('dbname');
+		$this->port = getenv('port');
+		$this->sslmode = getenv('sslmode');
+	}
+	  /*
+	 Function Name: getConnstring
+	 Params: NULL
+	 Created BY:
+	 Created ON : 
+	 Description : connection for Postgres server in Heroku
+   	 */
+	function getConnstring() { 
+		//Connecting to database
+		echo "host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode;die;
+		$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password." sslmode=".$this->sslmode."") or die("Connection failed: ".pg_last_error());
+		/* check connection */
+		if (pg_last_error()) {
+			 echo json_encode(
+				array("message" => "Connection failed")
+			  );
+			exit();
+		} else {
+			$this->conn = $con;
+		}
+		 // pg_close($con);
+		return $this->conn;
+	}
+
 ?>
